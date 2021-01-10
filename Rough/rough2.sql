65513319e8a894777cf1b599b8ce97d81e6ebcd9
Jabbar Azhar Mujeeb <67254298+azharmujeeb@users.noreply.github.com>
2021-01-10 11:26:39 +0530

    a

---
+++ Rough/rough2.sql
@@ -0,0 +1,13 @@
+use rough2
+
+select * from lib
+
+insert into lib (lno, name)
+values('2','mujeeb')
+select * from lib
+
+
+insert into Table_1 (lno, location)
+values('2','Ampara'),
+('1','akp')
+select * from Table_1
